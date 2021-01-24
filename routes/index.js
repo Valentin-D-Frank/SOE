d1b7178f561c2c6449ae26f4fd25b411aa7a7f20
EDGUS1 <eduardo.navarro2@unmsm.edu.pe>
2021-01-24 00:25:24 -0500

    data dinamica

--- routes/index.js
+++ routes/index.js
@@ -7,65 +7,71 @@ const api = express.Router();
 const dbConnection = require("../connect");
 const connection = dbConnection();
 
-api.all("*", function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Credentials", true);
-    next();
+api.all("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
 });
 
-api.get("/", function(req, res) {
-    res.render("principal", { nombre: "Eduardo" });
+api.get("/", function (req, res) {
+  res.render("principal");
 });
 
-api.get("/lista", function(req, res) {
-    res.render("lista", { nombre: "Eduardo" });
-});
+/* api.get("/lista", function (req, res) {
+  res.render("lista");
+}); */
 
 api.get("/prueba", (req, res) => {
-    connection.query("SELECT * FROM usuario", (err, result) => {
-        console.log(result);
-        res.render("login", {
-            user: result,
-        });
+  connection.query("SELECT * FROM usuario", (err, result) => {
+    console.log(result);
+    res.render("login", {
+      user: result,
     });
+  });
 });
 
 api.get("/curso", (req, res) => {
-    res.render("curso");
+  res.render("curso");
 });
 
-api.get("/crearCurso", function(req, res) {
-    res.render("crearCurso");
+api.get("/crearCurso", function (req, res) {
+  res.render("crearCurso");
 });
 
-api.get("/crearTarea", function(req, res) {
-    res.render("crearTarea");
+api.get("/crearTarea", function (req, res) {
+  res.render("crearTarea");
 });
 
-api.get("/login", function(req, res) {
-    res.render("login");
+api.get("/login", function (req, res) {
+  res.render("login");
 });
 
-api.get("/register", function(req, res) {
-    res.render("register");
+api.get("/register", function (req, res) {
+  res.render("register");
 });
 
-api.get("/dashboard", function(req, res) {
-    res.render("home");
+api.get("/dashboard", function (req, res) {
+  res.render("home");
 });
 
 api.get("/lista", (req, res) => {
-    res.render("lista");
+  const personas = [
+    { nombre: "Mark", apellido: "Otto" },
+    { nombre: "Jacob", apellido: "Thornton" },
+    { nombre: "Larry", apellido: "apellido" },
+    { nombre: "Eduardo", apellido: "Navarro" },
+  ];
+  res.render("lista", { personas });
 });
 
 api.post("/user", user);
-api.post('/homework', homework);
+api.post("/homework", homework);
 
-api.post('/course', course);
+api.post("/course", course);
 
 api.post("/login", (req, res) => {
-    res.send("Registrado");
+  res.send("Registrado");
 });
 
 /* api.use(function(req, res, next) {
