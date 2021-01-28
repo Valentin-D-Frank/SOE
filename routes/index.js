edcb8a765788199fdea1125a9b52f5d18eeb9c79
EDGUS1 <eduardo.navarro2@unmsm.edu.pe>
2021-01-28 00:07:27 -0500

    nombre_curso

--- routes/index.js
+++ routes/index.js
@@ -26,10 +26,11 @@ api.get("/curso/:id/:iduser", (req, res) => {
     let userid = [];
     connection.query("SELECT * FROM curso WHERE id = ?", [id], (err, result) => {
         userid = result[0].creador;
+        nombre_curso = result[0].nombre;
         connection.query("SELECT * FROM tarea WHERE id_curso = ?", [id], (err, result) => {
             profesor = false
             if (iduser == userid) profesor = true
-            res.render("curso", { result, profesor, curso_id: id, userid, iduser });
+            res.render("curso", { result, profesor, curso_id: id, userid, iduser, nombre_curso });
         });
     });
 });
